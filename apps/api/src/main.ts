2f9ec49c63b68311ce39aaff3b2a9ff1b6b8e81f
hubertinnowise <h2michalski@gmail.com>
2025-07-24 19:52:01 +0200

    Changes made after developing /auth module

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,36 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 import { ConfigService } from './config/config.service';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: { credentials: true, origin: '*' } });
+  const app = await NestFactory.create(AppModule, {
+    cors: { credentials: true, origin: '*' },
+  });
 
-    const configService = app.get(ConfigService);
+  const configService = app.get(ConfigService);
 
-    const port = configService.get('PORT');
-    const globalPrefix = 'api';
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
-    app.setGlobalPrefix(globalPrefix);
-    app.use(cookieParser());
+  app.use(cookieParser());
 
-    await app.listen(port, () => Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`));
+  const port = configService.get('PORT') || 3000;
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  await app.listen(port, () =>
+    Logger.log(`Application is running on: http://localhost:${port}/${globalPrefix}`),
+  );
 }
 
 bootstrap();
--- apps/api/src/modules/app.module.ts
+++ apps/api/src/modules/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '../config/config.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [ConfigModule],
+  imports: [ConfigModule, AuthModule],
 })
 export class AppModule {}
