6d4934f588375d18f5b4ff4b61ada6255f0860f3
hubertinnowise <h2michalski@gmail.com>
2025-08-01 10:46:33 +0200

    JwtModule added

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-    controllers: [AuthController],
-    exports: [PrismaService],
-    providers: [AuthService, PrismaService]
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_jwt_secret',
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService],
+  exports: [PrismaService],
 })
 export class AuthModule {}
