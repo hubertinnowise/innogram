402ad97e050c9b6bb0874ce9843144e857f7392f
hubertinnowise <h2michalski@gmail.com>
2025-08-19 18:40:26 +0200

    Removed old /apps/api as it contents got split into apps/api-user and apps/api-auth.

--- apps/api/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-    {
-        autorestart: true,
-        max_memory_restart: '1G',
-        name: 'api',
-        script: 'pnpm nx api:serve',
-    },
-];
--- apps/api/src/config/config.validation.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export type EnvSchema = z.infer<typeof envSchema>;
-export const envSchema = z.object({
-    PORT: z.coerce.number(),
-});
